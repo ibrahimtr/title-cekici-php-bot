9094e557c63db94a052e1a0ca1cb1dbb0950a932
ibrahim özdemir <ibrahimozdemirtrx@gmail.com>
2017-07-09 19:20:19 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,11 @@
 <?php 
 
 
-$icerik = file_get_contents("http://www.google.com");
-$baslik = ara("<title>", "</title>", $icerik);
-echo $baslik[0];
-
+$site=file_get_contents("http://www.google.com");
+preg_match_all('@<title>(.*?)</title>@si',$site,$baslik);
+echo $baslik[0][0];
+echo '<br>';
+echo 'veya';
+print_r($icerik);
 
 ?>
