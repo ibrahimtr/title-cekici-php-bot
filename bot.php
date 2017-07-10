c4d9f5356108143c70194c1f69182254e0e78392
ibrahim özdemir <ibrahimozdemirtrx@gmail.com>
2017-07-10 14:30:35 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,11 +1,6 @@
 <?php 
-
-
 $site=file_get_contents("http://www.google.com");
 preg_match_all('@<title>(.*?)</title>@si',$site,$baslik);
 echo $baslik[0][0];
-echo '<br>';
-echo 'veya';
-print_r($icerik);
-
+echo '<br>Sitenizin Basligina Bakin :)';
 ?>
